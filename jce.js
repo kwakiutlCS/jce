45596aad7a22091f56c6537ae1de61da4e4fc748
ricardo <ricardo.rsr.rodrigues@gmail.com>
2013-07-08 18:32:30 +0100

    code refactoring

--- jce.js
+++ jce.js
@@ -2,6 +2,7 @@ var engine = {
 
     depth: 1,
 
+    puzzle: 1,
 
     // gets all the possible moves in the board in a [["e2","e4"],["d2","d4"], ... ] format
     getPossibleMoves: function(position, turn) {
@@ -45,7 +46,7 @@ var engine = {
 	 }
 
 	 return moves;
-
+    
     },
 
 
@@ -72,73 +73,58 @@ var engine = {
    },
 
     
-    getEvaluation: function(pos, depth, color, turn) {
-	 console.log(depth);
-	 var other = "black";
-	 if (turn === "black")
-	     other = "white";
+    getEvaluation: function(pos, turn) {
+	 var other = turn === "black" ? "white" : "black";
 	 
+	 var depth = this.depth;
+
 	 if (depth === 1) {
 	     var possibleMoves = this.getPossibleMoves(pos, turn);
+            possibleMoves.sort(function() {return 0.5 - Math.random()}) 
 	    
-	     var score = 0;
+	     var score;
+	     
+	     var opponent_pos, m, r, possibleResponses, finalPosition, bestMove=false;
 
 	     for (var move in possibleMoves) {
-		  score += this.getHeuristic(chessBoard.generateNewPosition(possibleMoves[move][0],possibleMoves[move][1],"-",pos), turn);
-		  
-	     }
-	     score /= possibleMoves.length;
-	     
-	     return [score,""];
-	 }
-
-	 else {
-	     
-	     var result = chessBoard.getResult(pos, turn, "-");
-	     if (result === "white")
-		  return [1000,""];
-	     else if (result === "black")
-		  return [-1000,""];
-	     else if (result === "draw")
-		  return [0,""];
-	     
-	     
-	     if (color === turn) {
-		  
-		  
-		  var possibleMoves = this.getPossibleMoves(pos, turn);
-		  var bestMove = false;
-		  
-		  for (var m in possibleMoves) {
-		      
-		      var score = this.getEvaluation(chessBoard.generateNewPosition(possibleMoves[m][0],possibleMoves[m][1],"-",pos), depth-1,color,other);
-		      
-		      if (!bestMove || (turn === "white" && score[0] > bestMove[0]) || (turn === "black" && score[0] < bestMove[0]))
-			   bestMove = [score[0], possibleMoves[m]];
-		      else if (score[0] === bestMove[0] && Math.random() > 0.8)
-			   bestMove = [score[0], possibleMoves[m]];
+                score = 0;
+		  m = possibleMoves[move];
+		  opponent_pos = chessBoard.generateNewPosition(m[0], m[1], "-", chessBoard.position);
+                
+		  possibleResponses = this.getPossibleMoves(opponent_pos, other);
+                for (var response in possibleResponses) {
+                    r = possibleResponses[response];
+		      finalPosition = chessBoard.generateNewPosition(r[0], r[1], "-", opponent_pos);
+                    
+		      score += this.getHeuristic(finalPosition, turn); 
 		      
 		  }
+		  score /= possibleResponses.length;
 
-		  return bestMove;
+		  if (turn === "white") {
+		      if (!bestMove || score > bestMove[1])
+			   bestMove = [m,score];
+                }
+                else {
+		      if (!bestMove || score < bestMove[1])
+			   bestMove = [m,score];
+                }
+                
 	     }
 	     
-	     else {
-		  
-		  var possibleMoves = this.getPossibleMoves(pos, turn);
-		  var score = 0;
-		  
-		  for (var m in possibleMoves) {
-		      
-		      score += this.getEvaluation(chessBoard.generateNewPosition(possibleMoves[m][0],possibleMoves[m][1],"-",pos), depth-1,color,other)[0];
-		      		      
-		  }
-		  
-		  return [score/possibleMoves.length,""];
-	     }
-	     
+	     console.log(score);
+	     console.log(bestMove[0]);
+	     return bestMove[0];
 	 }
+
+	 
+	 
 
 	 
     },
+
+
+    
 }
+
+
